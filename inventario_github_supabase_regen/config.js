5726fe13a309fd484a8cb55f49aeab8e0eb1176c
Ramon A.C.Vega <raymonchu40@gmail.com>
2026-03-11 21:31:55 +0100

    Delete inventario_github_supabase_regen directory

--- inventario_github_supabase_regen/config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL:"https://qzwauzyeewmgetkrhypz.supabase.co", 
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6d2F1enllZXdtZ2V0a3JoeXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNTcwNzUsImV4cCI6MjA4ODgzMzA3NX0.uHjJEr-z5rzkMXIBBJlmrE7yAapnY9X2DnKtoHXL3cI" 
-};
